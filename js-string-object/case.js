05ecbffb21e2aefcabbf3974f1ce80aac1ebcb58
ibnult <ibnultahsinrihan@gmail.com>
2025-06-02 13:17:25 +0600

    second  commit

---
+++ js-string-object/case.js
@@ -0,0 +1,9 @@
+const subject = 'JavaScript: The Good Parts';
+const book = 'JavaScript: The Good Parts';
+
+if(subject.toLowerCase() === book.toLowerCase()){
+  console.log('The subject is JavaScript');
+}
+else {
+  console.log('The subject is not JavaScript');
+}
