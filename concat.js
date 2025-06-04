421dbcbe23ae3a6119aa4ac46906b49bc845e3af
ibnult <ibnultahsinrihan@gmail.com>
2025-06-04 12:46:31 +0600

    concat() practice complete

---
+++ concat.js
@@ -0,0 +1,10 @@
+const first = 'Abid';
+const last = 'Hassan';
+
+const fullName = first + ' ' + last;
+console.log(fullName); // Output: Abid Hassan
+
+//using concat method
+const fullNameConcat = first.concat(' ', last);
+console.log(fullNameConcat); // Output: Abid Hassan
+// Note: The concat method is not necessary here, but it can be used for demonstration.
