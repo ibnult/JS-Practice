ffee9f40a06b944e17f9e9304e7059737ef253bf
ibnult <ibnultahsinrihan@gmail.com>
2025-06-05 13:47:53 +0600

    reverse() practice complete

--- reverse.js
+++ reverse.js
@@ -4,11 +4,19 @@ let reverse='';
 for (const letter of sentence) {
   reverse = letter + reverse;
 }
-console.log(reverse); // Output: tpircSavaJ gninrael ma I
+ console.log(reverse); // Output: tpircSavaJ gninrael ma I
 
 //for loop
 //ignore 
+let rev='';
 for (let i=0; i<sentence.length; i++) {
   //console.log(i);
-  console.log(sentence[i]);
+  //console.log(sentence[i]);
+  const letter = sentence[i];
+  rev = letter + rev;
 }
+console.log(rev); // Output: tpircSavaJ gninrael ma I
+
+//shortcut
+const reversed= sentence.split('').reverse().join('');
+console.log(reversed); // Output: [ 'I am learning JavaScript' ]
