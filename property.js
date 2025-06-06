3e2bcf57bedc3e02eab94375e1c1adc71e037d17
ibnult <ibnultahsinrihan@gmail.com>
2025-06-07 01:38:23 +0600

    property  practice complete

---
+++ property.js
@@ -0,0 +1,12 @@
+const person={
+    name: "John",
+    age: 30,
+    profession: "Engineer",
+    salary: 50000,
+    married: true,
+}
+
+//dot notation
+console.log(person.name); // John
+// bracket notation
+console.log(person["age"]); // 30
