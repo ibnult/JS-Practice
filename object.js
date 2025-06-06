94695b1d11e5bda4af3df76d4f107e63fbbc5154
ibnult <ibnultahsinrihan@gmail.com>
2025-06-07 01:18:18 +0600

    object  practice complete

---
+++ object.js
@@ -0,0 +1,21 @@
+//primitives
+   const isPractical = true;
+   const duration = 45;
+   const schoolName = "Greenwood High";
+
+    
+    //object
+    //non-primitive
+const bottle ={
+    brand: "Coca-Cola",
+    color: "Red",
+    price: 45,
+    isClean: false,
+}
+
+const subject = {
+    name:'biology',
+    teacher: 'Mr. Smith',
+    examDate: '30 Feb',
+    chapters:['first chapter', 'second chapter', 'third chapter'],
+}
