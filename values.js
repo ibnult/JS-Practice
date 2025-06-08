80d7e80386effaec51d087abbfab9202b796e216
ibnult <ibnultahsinrihan@gmail.com>
2025-06-09 00:50:31 +0600

    values  practice complete

---
+++ values.js
@@ -0,0 +1,9 @@
+const computer={
+    brand: "Dell",
+    price: 1200,
+    processor:'intel',
+    hdd:'1TB',
+}
+
+const values = Object.values(computer);
+console.log(values); // Output: [ 'Dell', 1200, 'intel', '1TB' ]
