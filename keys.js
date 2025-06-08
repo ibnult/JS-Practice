fc8de31ef01904f9f751c7115253562eb2d04be6
ibnult <ibnultahsinrihan@gmail.com>
2025-06-08 12:39:39 +0600

    keys  practice complete

---
+++ keys.js
@@ -0,0 +1,9 @@
+const computer={
+    brand: "Dell",
+    price: 1200,
+    processor:'intel',
+    hdd:'1TB',
+}
+
+const keys = Object.keys(computer);
+console.log(keys); // Output: [ 'brand', 'price', 'processor', 'hdd' ]
