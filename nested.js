57bbc7397bc4cc85a842d0cac490da390052ec19
ibnult <ibnultahsinrihan@gmail.com>
2025-06-09 18:38:21 +0600

    nested values  practice complete

--- nested.js
+++ nested.js
@@ -11,5 +11,13 @@ const college= {
     }
 }
 
-console.log(college.unique.result.merit); // VNC
+console.log(college.unique.result.merit); // Object property access
 
+college.unique.result.merit = 'topper'; // Object property assignment
+console.log(college.unique.result.merit); // Object property access after assignment
+
+
+college.events[1]= 'victory day'; // Array element assignment
+console.log(college.events[1]); // Array element access after assignment
+delete college.class;
+console.log(college); // Deleting a property from an object
