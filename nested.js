f5969c01dce17693420c7dc40f70cabc6ac86803
ibnult <ibnultahsinrihan@gmail.com>
2025-06-09 17:01:21 +0600

    nested values  practice complete

---
+++ nested.js
@@ -0,0 +1,15 @@
+const college= {
+    name : 'VNC',
+    class: ['11', '12'],
+    events:['science fair', 'bijoy dibos', '21 Feb'],
+    unique:{
+        color: 'blue',
+        result: {
+            gpa:5,
+            merit: 'top',
+        }
+    }
+}
+
+console.log(college.unique.result.merit); // VNC
+
