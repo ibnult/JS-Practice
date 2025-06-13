4583c031cd42f782bf0fe0c0076bff7da014876a
ibnult <ibnultahsinrihan@gmail.com>
2025-06-14 02:13:01 +0600

    reverse array practice

--- reverseArray.js
+++ reverseArray.js
@@ -10,3 +10,12 @@ for(const num of numbers){
 }
 
 console.log(rev_numbers); // Output: [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+
+//reverse side
+const rev_rev_numbers=[];
+for(let i=numbers.length-1;i>=0;i--){
+    const num=numbers[i];
+    rev_rev_numbers.push(num);
+}
+console.log(rev_rev_numbers); // Output: [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+
