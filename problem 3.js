79199b41f7df2aa1e11e90b0a25cc16f61baa287
ibnult <ibnultahsinrihan@gmail.com>
2025-06-13 19:59:28 +0600

    problem 3  completed

--- problem 3.js
+++ problem 3.js
@@ -2,9 +2,13 @@ let price ='$249';
 
 let chars =price.split("");
 
+let sum=0;
+
 for( let i = 0; i < chars.length; i++) {
     let element = chars[i];
     if(!isNaN(element) && element !==" ") {
-        console.log(element);
+        sum += parseInt(element);
     }
 }
+
+console.log(sum); // Output: 15
