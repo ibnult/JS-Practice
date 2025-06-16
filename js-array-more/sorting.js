a0b54981a3ced3d4c9859f8be8b4ce89fba03d2b
ibnult <ibnultahsinrihan@gmail.com>
2025-06-16 13:48:30 +0600

    sorting done

--- js-array-more/sorting.js
+++ js-array-more/sorting.js
@@ -3,12 +3,15 @@ const sortedPersons = persons.sort();
 console.log(sortedPersons);
 
 //sort 
-const numbers=[4,7,2,8,3,6];
 
 /*
 Ascending order----> smaller to larger: [2, 3, 4, 6, 7, 8]
 Descending order----> larger to smaller : [8, 7, 6, 4, 3, 2]
 */
+const numbers = [4,7,12,8,43,6,1];
+// const numbers_asc = numbers.sort() not work for numbers
+const numbers_asc = [...numbers].sort((a, b) => a - b); // Ascending order
+const numbers_desc = [...numbers].sort((a, b) => b - a); // Descending order
 
-const numbers_asc = numbers.sort()
-console.log(numbers_asc); // [2, 3, 4, 6, 7, 8]
+console.log(numbers_asc);
+console.log(numbers_desc);
