d65013cbf7adcc549c1a8a3ec7de25b296ef6eaa
ibnult <ibnultahsinrihan@gmail.com>
2025-06-17 12:43:27 +0600

    task 4 complete

---
+++ array looping task/task 4.js
@@ -0,0 +1,9 @@
+const statement = 'I am a hard working person';
+
+const words = statement.split(' ');
+
+const reversedWords = words.reverse();
+
+const reversedSentence = reversedWords.join(' ');
+
+console.log(reversedSentence); 
