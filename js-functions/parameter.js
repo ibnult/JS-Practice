d1ff1122d36050afd8ea100eeb89b874a6337705
ibnult <ibnultahsinrihan@gmail.com>
2025-06-21 16:26:17 +0600

    parameters

---
+++ js-functions/parameter.js
@@ -0,0 +1,13 @@
+function square(number){
+    const borgo =number*number;
+    console.log('Square if the given number is: ',borgo);
+}
+
+square(5); 
+
+function add(num1, num2){
+    const sum= num1 + num2;
+    console.log('Sum of the two numbers is: ', sum);
+}
+
+add(10,20);
