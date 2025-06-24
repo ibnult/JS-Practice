592761689ef48053b0ce293850c22cb70582352c
ibnult <ibnultahsinrihan@gmail.com>
2025-06-24 22:10:54 +0600

    array operation

---
+++ js-functions/array-operation.js
@@ -0,0 +1,14 @@
+/**
+ * objective: write a function to give me the sum of all numbers  in an array
+ * step-1: declare a function
+ * step-2: call check whether the function is called properly
+ *  step-3: set a parameter(S)
+ * step-4: pass the parameter(S) ,  check whether the parameter is passed properly
+ * step-5: do the function tasks (step by step)
+ */
+function sumOfNumbers (number){
+console.log(number);
+}
+const numbs=[1,2,3,4,5];
+const sum=sumOfNumbers(numbs);
+console.log('Sum of the numbers is: ' + sum); 
