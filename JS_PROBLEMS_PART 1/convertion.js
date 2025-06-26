bf7be9f9a272798e3b718f79ce2b3743ffdf91c3
ibnult <ibnultahsinrihan@gmail.com>
2025-06-27 00:09:51 +0600

    convertion  done

--- JS_PROBLEMS_PART 1/convertion.js
+++ JS_PROBLEMS_PART 1/convertion.js
@@ -5,5 +5,18 @@ function inchToFeet (inch){
     return feet;
 }
 
+//75 inch
+
+function inchToFeet2 (inch){
+    const feetFraction =inch/12;
+    const feetNumber=parseInt(feetFraction);  
+    const inchRemaining= inch%12;
+    const result =feetNumber + " feet " + inchRemaining + " inch";
+    return result; 
+}
+
+
 const rihanHeight= inchToFeet(75);
-console.log(rihanHeight);
+//console.log(rihanHeight);
+const rihanHeight2= inchToFeet2(75);
+console.log(rihanHeight2); 
