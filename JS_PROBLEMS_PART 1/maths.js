30fa001df368f741213f5fe497d24bf301dae897
ibnult <ibnultahsinrihan@gmail.com>
2025-06-28 20:35:02 +0600

    math function

---
+++ JS_PROBLEMS_PART 1/maths.js
@@ -0,0 +1,16 @@
+const min = Math.min(1,2,3,4,5);
+console.log(min);
+const max= Math.max(1,2,3,4,5);
+console.log(max);
+
+
+console.log(Math.PI);
+console.log(Math.abs(5-10));
+console.log(Math.round(2.85))
+console.log(Math.floor(4.85))
+console.log(Math.ceil(6.85))
+
+console.log(Math.random())
+console.log(Math.round()*10)
+const rand=Math.round(Math.random()*10)
+console.log(rand)
