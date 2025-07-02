3921cf8c7f63c5edc677c59ff61d934deea67aab
ibnult <ibnultahsinrihan@gmail.com>
2025-07-02 23:17:19 +0600

    max finding done

---
+++ JS_PROBLEMS_PART_2/max3.js
@@ -0,0 +1,13 @@
+const jim = 56;
+const tim =89;
+const kim =68;
+
+if(jim> tim&& jim> kim){
+    console.log("Jim is the greatest");
+}
+else if(tim> jim && tim> kim){
+    console.log("Tim is the greatest");
+}
+else{
+    console.log("Kim is the greatest");
+}
