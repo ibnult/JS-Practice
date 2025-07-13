41ede1f93456fc741a80f8b47ef857d24a071545
ibnult <ibnultahsinrihan@gmail.com>
2025-07-13 20:39:11 +0600

    more pbject

--- js-string-object/object1.js
+++ js-string-object/object1.js
@@ -4,6 +4,10 @@ const student = {
     address:'movie cinema hall',
     isSingle: true,
     friends: ['Shakib', 'Siam', 'Siam Ahmed'],
+    movies: [{name: 'Dhaka Attack', year: 2017}, {name: 'Nolok', year: 2015}],
+    act:function() {
+        console.log('Acting in a movie');
+    },
     car:{
         brand:'G Wagon',
         price: 1000000,
@@ -11,4 +15,6 @@ const student = {
     }
 };
 
-console.log(student);
+//console.log(student);
+console.log(student.act);
+student.act();
