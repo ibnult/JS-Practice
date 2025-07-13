05ab867dfbb4c3738e5794e62bb9c0b7337a610d
ibnult <ibnultahsinrihan@gmail.com>
2025-07-13 20:25:59 +0600

    more pbject

---
+++ js-string-object/object1.js
@@ -0,0 +1,14 @@
+const student = {
+    name: 'Sakib Khan',
+    id:121,
+    address:'movie cinema hall',
+    isSingle: true,
+    friends: ['Shakib', 'Siam', 'Siam Ahmed'],
+    car:{
+        brand:'G Wagon',
+        price: 1000000,
+        model: '2023',
+    }
+};
+
+console.log(student);
